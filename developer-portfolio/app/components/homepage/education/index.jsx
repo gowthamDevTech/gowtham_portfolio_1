157ea858c492dbf9bd8d1589de3dd661ae85fef9
rumi-dev <rumidevscripted@gmail.com>
2025-03-10 19:18:52 +0530

    changes

--- developer-portfolio/app/components/homepage/education/index.jsx
+++ developer-portfolio/app/components/homepage/education/index.jsx
@@ -47,7 +47,7 @@ function Education() {
             <div className="flex flex-col gap-6">
               {educations.map((education) => (
                 // <GlowCard key={education.id} identifier={`education-${education.id}`}>
-                <div className="p-3 relative text-white">
+                <div className="p-3 relative text-white" key={education.id}>
                   <Image
                     src="/blur-23.svg"
                     alt="Hero"
