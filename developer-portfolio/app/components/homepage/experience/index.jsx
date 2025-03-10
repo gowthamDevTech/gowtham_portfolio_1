d831d12f6bcae5fa9bef7d9e6befeaec58a98715
rumi-dev <rumidevscripted@gmail.com>
2025-03-10 19:16:32 +0530

    changes

--- developer-portfolio/app/components/homepage/experience/index.jsx
+++ developer-portfolio/app/components/homepage/experience/index.jsx
@@ -43,38 +43,38 @@ function Experience() {
           <div>
             <div className="flex flex-col gap-6">
               {experiences.map((experience) => (
-                <GlowCard
-                  key={experience.id}
-                  identifier={`experience-${experience.id}`}
-                >
-                  <div className="p-3 relative">
-                    <Image
-                      src="/blur-23.svg"
-                      alt="Hero"
-                      width={1080}
-                      height={200}
-                      className="absolute bottom-0 opacity-80"
-                    />
-                    <div className="flex justify-center">
-                      <p className="text-xs sm:text-lg text-[#16f2b3]">
-                        {experience.duration}
+                // <GlowCard
+                //   key={experience.id}
+                //   identifier={`experience-${experience.id}`}
+                // >
+                <div className="p-3 relative" key={experience.id}>
+                  <Image
+                    src="/blur-23.svg"
+                    alt="Hero"
+                    width={1080}
+                    height={200}
+                    className="absolute bottom-0 opacity-80"
+                  />
+                  <div className="flex justify-center">
+                    <p className="text-xs sm:text-lg text-[#16f2b3]">
+                      {experience.duration}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-x-8 px-3 py-5">
+                    <div className="text-violet-500  transition-all duration-300 hover:scale-125">
+                      <BsPersonWorkspace size={36} />
+                    </div>
+                    <div>
+                      <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                        {experience.title}
+                      </p>
+                      <p className="text-sm sm:text-base">
+                        {experience.company}
                       </p>
                     </div>
-                    <div className="flex items-center gap-x-8 px-3 py-5">
-                      <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                        <BsPersonWorkspace size={36} />
-                      </div>
-                      <div>
-                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                          {experience.title}
-                        </p>
-                        <p className="text-sm sm:text-base">
-                          {experience.company}
-                        </p>
-                      </div>
-                    </div>
                   </div>
-                </GlowCard>
+                </div>
+                // </GlowCard>
               ))}
             </div>
           </div>
